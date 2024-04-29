a984ca9da42cb7f8e0835cf4bf2c862bad1ad296
maricovp <marcos2015vinicuis@gmail.com>
2024-04-29 11:06:23 -0400

    adicionada atividade3

---
+++ atividade3.js
@@ -0,0 +1,6 @@
+/*Questão 03: Escreva uma arrow function chamada somaArray que recebe um array
+de números como parâmetro e retorna a soma de todos os elementos desse array.*/
+function soma (a, b) {
+    return a + b;
+    }
+    console.log(soma(5, 7));
