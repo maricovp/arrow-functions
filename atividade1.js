a7f9b5928b2e6310eb057228cd15413193f3e420
maricovp <marcos2015vinicuis@gmail.com>
2024-04-29 11:06:12 -0400

    adicionada atividade1

---
+++ atividade1.js
@@ -0,0 +1,4 @@
+/*Questão 01: Escreva uma arrow function chamada dobro que recebe um número
+como parâmetro e retorna o dobro desse número.*/
+const quadrado = x => x + x;
+console.log(quadrado(4));
