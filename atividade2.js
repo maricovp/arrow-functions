e7ddf565ae2801a1d8e28bde08b97ee92cc7f146
maricovp <marcos2015vinicuis@gmail.com>
2024-04-29 11:06:18 -0400

    adicionada atividade2

---
+++ atividade2.js
@@ -0,0 +1,6 @@
+/*Questão 02: Escreva uma arrow function chamada saudacao que recebe um nome
+como parâmetro e retorna uma string de saudação.*/
+function saudacao(nome) {
+    console.log("Olá, " + nome + "!");
+    }
+    saudacao("Marcos");
