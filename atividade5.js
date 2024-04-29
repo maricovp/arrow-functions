6057470629ba91d250319ed3475f21a7aea7101f
maricovp <marcos2015vinicuis@gmail.com>
2024-04-29 11:06:32 -0400

    adicionada atividade5

---
+++ atividade5.js
@@ -0,0 +1,7 @@
+/*Questão 05: Escreva uma arrow function chamada maiorNumero que recebe dois
+números como parâmetros e retorna o maior deles.*/
+
+const maiorNumero = (x, b) => {
+    return Math.max(x, b)
+    };
+    console.log(maiorNumero(5, 8));
